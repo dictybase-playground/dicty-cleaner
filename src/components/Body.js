cda92c0ab7966aa746277a43b4c2cb4d334e05dc
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-02-22 22:13:45 -0600

    Add grid

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import Grid from "material-ui/Grid";
+import Divider from "material-ui/Divider";
+import "typeface-roboto";
 
 const Body = props => {
   return (
-    <p>Hello</p>
+    <div>
+      <Grid container spacing={24}>
+        <Grid item xs={12} sm={6}>
+          Grid section 1
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          Grid section 2
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
