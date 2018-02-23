64f066f62d6726b2fdcdcf67bc73a67036903500
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-02-22 23:17:07 -0600

    Override text field style to create text box

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
-import TextField from 'material-ui/TextField';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+
+import TextField from "material-ui/TextField";
 import Button from "material-ui/Button";
+import Input, { InputLabel } from "material-ui/Input";
+
+
+const styles = {
+  textInput: {
+    borderRadius: 4,
+    // backgroundColor: theme.palette.common.white,
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    padding: "10px 12px",
+    width: "calc(100% - 20px)",
+    // transition: theme.transitions.create(["border-color", "box-shadow"]),
+    // "&:focus": {
+    //   borderColor: "#80bdff",
+    //   boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)"
+    // }
+  },
+};
 
 const Body = props => {
   return (
     <div>
       <center>
-        <TextField multiline rows={10} />
+        <TextField
+          magin="normal"
+          multiline
+          rows={10}
+          className={props.classes.textInput}
+        />
         <div>
           <Button variant="raised" size="large" color="primary">
             Submit
@@ -17,4 +43,9 @@ const Body = props => {
   );
 };
 
-export default Body;
+Body.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Body);
