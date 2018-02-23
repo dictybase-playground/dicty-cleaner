372ceb7e7b3d13bcd4cd15970105ece0a1739119
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-02-22 22:23:36 -0600

    Create basic text field

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Grid from "material-ui/Grid";
 import Divider from "material-ui/Divider";
 import Button from "material-ui/Button";
+import TextField from 'material-ui/TextField';
 
 import "typeface-roboto";
 
@@ -12,9 +13,12 @@ const Body = props => {
       <Grid container spacing={24}>
         <Grid item xs={12} sm={6}>
           <center>
+            <TextField multiline />
+          </center>
+          <center>
             <Button variant="raised" size="large" color="primary">
               Submit
-            </Button>            
+            </Button>
           </center>
 
         </Grid>
