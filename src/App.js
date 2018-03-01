e733a84a249452778339adfd020712afbacbeff8
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-03-01 00:03:23 -0600

    Remove header and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Header from './components/AppHeader';
-import Footer from './components/AppFooter';
 import Body from './components/Body';
 
 
@@ -8,9 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
         <Body />
-        <Footer />
       </div>
     );
   }
